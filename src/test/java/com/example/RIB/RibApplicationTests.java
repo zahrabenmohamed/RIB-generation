f834f08c6cbe1957814c347e1a3778db274f1b31
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-26 20:52:42 +0100

    init commit

---
+++ src/test/java/com/example/RIB/RibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RIB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

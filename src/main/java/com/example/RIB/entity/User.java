97c6483ea7b5575bee518bc841317b0b81dd7a7b
zahrabenmohamed <benmedzahra20@gmail.com>
2023-02-26 21:51:48 +0100

    create user entity and update it

--- src/main/java/com/example/RIB/entity/User.java
+++ src/main/java/com/example/RIB/entity/User.java
@@ -1,28 +1,26 @@
 package com.example.RIB.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
-@Table(name="RIB")
-public class RIB {
+@Table(name="USER")
+@Data
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user")
     private String user;
 
-    @Column(name = "bin")
     private String bin;
 
-    @Column(name = "iban")
     private String iban;
 
-    @Column(name = "address")
     private String address;
 
-    @Column(name = "tel")
     private String tel;
 
 
